d996dfb001ff47e61a9687119dd25354187acc87
Alexey <pavlof01@icloud.com>
2025-12-02 23:41:42 +0400

    feat: add tm lang for JSC and SpiderMonkey

---
+++ js-bytecode-web/components/OutputsPanel/jsc-opcodes.ts
@@ -0,0 +1,13 @@
+
+export const OPCODE_INFO = {
+  enter: "Establish a new stack frame for a function.",
+  mov: "Move a value from a source to a destination register.",
+  "new_func": "Create a new function object from a function declaration.",
+  "new_func_exp": "Create a new function expression.",
+  "new_async_func": "Create a new async function object.",
+  "new_generator_func": "Create a new generator function object.",
+  "resolve_scope": "Resolve a variable's scope.",
+  "put_to_scope": "Put a value into a scope.",
+  "new_object": "Create a new empty object.",
+  "define_data_property": "Define a data property on an object.",
+};
